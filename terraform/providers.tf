facf311ce08a35e2e3224200d36c207a1111d8a5
Siddhant Sharma <siddhants29@gmail.com>
2025-08-14 18:39:40 +0530

    Intial Commit

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,12 +1,25 @@
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = ">=3.0"
+#     }
+#   }
+# }
+# #
+# # provider "azurerm" {
+# #   features = {}
+# # }
+provider "azurerm" {
+  features {}
+}
+
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.0"
+      version = "~> 3.0"
     }
   }
+  required_version = ">= 1.0"
 }
-#
-# provider "azurerm" {
-#   features = {}
-# }
